0338a4ad9e7adbe81cd32744b5c1aae60d44d5b6
julrombel-sys <jul.rombel@gmail.com>
2026-04-11 20:46:38 +0200

    Add edge_exists function declaration to graph.h

--- c/src/graph.h
+++ c/src/graph.h
@@ -6,6 +6,7 @@ typedef struct Graph Graph;
 
 Graph create_graph(int max_edges);
 void add_edge(Graph *g, int u, int v, double w);
+int edge_exists(Graph *g, int u, int v);
 void free_graph(Graph *g);
 
 #endif
