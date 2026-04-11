a78d216651aa1ef698d89358a3a463541abc5713
julrombel-sys <jul.rombel@gmail.com>
2026-04-11 15:47:20 +0200

    Implement graph data structure and functions

---
+++ c/src/graph.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "graph.h"
+
+Graph create_graph(int max_edges) {
+    Graph g;
+    g.n_vertices = 0;
+    g.n_edges = 0;
+    g.edges = malloc(sizeof(Edge) * max_edges);
+    return g;
+}
+
+void add_edge(Graph *g, int u, int v, double w) {
+    g->edges[g->n_edges].u = u;
+    g->edges[g->n_edges].v = v;
+    g->edges[g->n_edges].w = w;
+
+    g->n_edges++;
+
+    if (u > g->n_vertices) g->n_vertices = u;
+    if (v > g->n_vertices) g->n_vertices = v;
+}
+
+void free_graph(Graph *g) {
+    free(g->edges);
+}
