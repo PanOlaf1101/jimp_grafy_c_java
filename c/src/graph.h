5d1ccc9d528749c04eec4f654c5191317fec13bd
julrombel-sys <jul.rombel@gmail.com>
2026-04-11 20:31:25 +0200

    Use forward declarations for Edge and Graph structures
    Updated graph.h to utilize forward declarations for Edge and Graph structures, improving clarity and reducing dependencies.

--- c/src/graph.h
+++ c/src/graph.h
@@ -1,17 +1,8 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-typedef struct {
-    int u;
-    int v;
-    double w;
-} Edge;
-
-typedef struct {
-    int n_vertices;
-    int n_edges;
-    Edge *edges;
-} Graph;
+typedef struct Edge Edge;
+typedef struct Graph Graph;
 
 Graph create_graph(int max_edges);
 void add_edge(Graph *g, int u, int v, double w);
