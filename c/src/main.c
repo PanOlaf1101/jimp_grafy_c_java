b38feacb7082ed5337226d4e4e9e027e3ee97309
julrombel-sys <jul.rombel@gmail.com>
2026-04-12 19:30:52 +0200

    Modify read_config and implement triangulation
    Updated read_config call and added triangulation logic.

--- c/src/main.c
+++ c/src/main.c
@@ -18,8 +18,34 @@ void cleanup(void) {
 int main(int argc, char *argv[]) {
 	setlocale(LC_CTYPE, "C.UTF-8");
 
-	read_config(argv+1, argc-1);
+	read_config(argv, argc); //zamniast gv+1 i gc-1
 
+    // testowy main-użycie triangulacji 
+    Graph g = create_graph(1000);
+
+    char name[32];
+    int u, v; 
+    double w; //etukiety , wieszchołkiy i waga-odległość
+
+    while (fscanf(conf.input_file, "%s %d %d %lf", //czyta format
+                  name, &u, &v, &w) == 4)
+    {
+        add_edge(&g, u, v, w); 
+    }
+
+    triangulate(&g); //z triangulation.c
+
+    for (int i = 1; i <= g.n_vertices; i++)   //output
+    {
+        fprintf(conf.output_file, "%d %.6f %.6f\n",
+                g.vertices[i].id,
+                g.vertices[i].x,
+                g.vertices[i].y);
+    }
+
+    free_graph(&g); // koniec programu i grafu
+
+	
 	cleanup();
 	return 0;
 }
