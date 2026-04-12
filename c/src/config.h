5d4d0c0ba433d826fe30d22ba569aaf10327a7d7
julrombel-sys <jul.rombel@gmail.com>
2026-04-12 17:25:23 +0200

    Add global variable declaration for Config
    Declare global variable 'conf' in config.h

--- c/src/config.h
+++ c/src/config.h
@@ -9,6 +9,8 @@ struct Config {
 #define ALG_SL 1 //Spectral laytout
 #define ALG_TR 2 //Triangulation
 
+extern struct Config conf; // Deklaracja globalnej zmiennej
+
 void read_config(char *arg[], int n);
 
 #endif //_CONFIG_H__
